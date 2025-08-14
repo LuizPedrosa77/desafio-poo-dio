d1584c689179c64ae1b537d9120e880c3d35e122
Gustavo Pedrosa <luizgfju@hotmail.com>
2025-08-13 21:14:18 -0300

    final

--- src/main/java/br/com/dio/desafio/Main.java
+++ src/main/java/br/com/dio/desafio/Main.java
@@ -1,12 +1,12 @@
 package br.com.dio.desafio;
 
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,6 +29,12 @@ public class Main {
         curso2.setDescricao("Descrição curso de JS");
         curso2.setCargaHoraria(8);
 
+        System.out.println();
+        System.out.println("----- CONTEUDO -----");
+        System.out.println();
+        Conteudo conteudo1 = new Curso(); // Polimorfismo
+
+
         System.out.println(curso2);
         System.out.println();
         System.out.println("-----  MENTORIA -----");
@@ -43,6 +49,40 @@ public class Main {
 
         System.out.println(mentoria1);
         //System.out.println(mentoria1.getData().format(dataMentoriaFormatada));
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+
+        Dev devGustavo = new Dev();
+        devGustavo.setNome("Gustavo");
+        devGustavo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos: "+devGustavo.getConteudoInscritos());
+        devGustavo.progredir();
+        devGustavo.progredir();
+        devGustavo.progredir();
+        System.out.println("-");
+        System.out.println("Conteudo Inscritos: "+devGustavo.getConteudoInscritos());
+        System.out.println("Conteudo Concluídos: "+devGustavo.getConteudoConcluidos());
+        System.out.println("XP: "+devGustavo.calcularTotalXp());
+
+
+        Dev devLuiz = new Dev();
+        devLuiz.setNome("Luiz");
+        devLuiz.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos: "+devLuiz.getConteudoInscritos());
+        devLuiz.progredir();
+        devLuiz.progredir();
+        devLuiz.progredir();
+        System.out.println("-");
+        System.out.println("Conteudo Inscritos: "+devLuiz.getConteudoInscritos());
+        System.out.println("Conteudo Concluídos: "+devLuiz.getConteudoConcluidos());
+        System.out.println("XP: "+devLuiz.calcularTotalXp());
+
 
 
 
--- src/main/java/br/com/dio/desafio/dominio/Mentoria.java
+++ src/main/java/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,10 +2,8 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
-    private String titulo;
-    private String descricao;
     private LocalDate data;
 
     public Mentoria() {
@@ -24,23 +22,13 @@ public class Mentoria {
         this.data = data;
     }
     */
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
     //Get e Set
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -53,8 +41,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
